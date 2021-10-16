a1a4cde08f76495998b1c7453106fbbc4373eb5a
Kesha Devkar <91537119+Kesha1329@users.noreply.github.com>
2021-10-16 17:48:52 +0000

    qz1

--- index.js
+++ index.js
@@ -2,26 +2,20 @@ var rs = require("readline-sync");
 const chalk = require("chalk");
 
 var score = 0;
-var highscore = 30;
-
+var highscore = 35;
 const questions = require("./data");
-
-
 let green = chalk.bold.green;
 let gray = chalk.bold.gray;
 let red = chalk.bold.red;
 let cyan = chalk.bold.cyan;
 let yellow = chalk.bold.yellow;
-let title = chalk.black.bold.bgYellow;
-let pink =chalk.bold.magenta;
+let bg = chalk.black.bold.bgOrange;
+let magenta =chalk.bold.magenta;
 
-
-
-console.log(title("Welcome to Quiz \n"));
+console.log(bg("--: Welcome to Exercise Quiz On Health And Fitness :-- \n"));
 let user = rs.question("Enter Your Name : ");
-console.log(green(`Hello ${user}`));
+console.log(green(`\n Hello ${user}`));
 
-console.log(gray("\nHere's a bunch of funny tricky questions that will really get your brain. So, let's get your mind ready for the mess up!"));
 console.log("\nThere will be 5 questions.");
 console.log(green("+5"), "points for correct answer.", red("-2"), "points for wrong answer.\n");
 
@@ -32,29 +26,23 @@ for (q of questions) {
   console.log(cyan('C: ' + q.C));
   console.log(cyan('D: ' + q.D));
 
-
-  //input answer
   var ans = rs.question('Answer : ');
-
-  //compare answer
   if (ans.toUpperCase() === q.answer) {
     console.log(green("\n+5"), "Your answer is", green("correct"), ".");
     score += 5;
-    console.log("Your current score is", green(score), "\n");
+    console.log("Your current score is", magenta(score), "\n");
   } else {
     console.log(red("-2"), "Your answer is", red("incorrect"), ".");
     console.log("Correct Answer :", green(q.answer));
     score -= 2;
-    console.log("Your current score is", red(score), "\n");
+    console.log("Your current score is", magenta(score), "\n");
   }
 }
-
-//check the performance
 if (score > highscore) {
-  console.log(green("Congratulation..!You Beat the best score."))
+  console.log(green("Congratulation... \n You Beat the best score."))
 } else {
-  console.log(red("You couldn't beat the highscore . Better luck next time!\n"))
+  console.log(red("You couldn't beat the highscore. Better luck next time!\n"))
 }
 
-console.log(yellow("Scoreboard:"))
-console.log('Hello ' + pink(user) + ' Your final score is : ' + score)
+console.log(yellow("\n ---: Scoreboard :---"))
+console.log(  magenta(user) + ' Your final score is : ' + magenta(score))
